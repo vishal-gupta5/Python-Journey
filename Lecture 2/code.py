95cf1f01a71c6cb69c410345c7c22f4b7d5f3d56
vishal-gupta5 <guptaaryan1103@gmail.com>
2026-09-04 20:02:51 +0530

    How to make condtional statements

--- Lecture 2/code.py
+++ Lecture 2/code.py
@@ -1,1 +1,21 @@
-# Second Lecture
+# Condional Statements (if, elif, else)
+
+age = int(input("Enter the age: "))
+
+if age >= 18:
+    print("You are eligible!")
+else:
+    print("You are not eligible!")
+    
+    
+    
+#! Traffic Light program
+
+colour= input("Enter the colour: ")
+
+if (colour == "red"):
+    print("Stop!")
+elif (colour == "yellow"):
+    print("Ready to go!")
+else:
+    print("Go!")
