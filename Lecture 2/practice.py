44a10972a01286b09c5411b8f4c6991a6447c74a
vishal-gupta5 <guptaaryan1103@gmail.com>
2026-09-04 20:21:52 +0530

    Check the n is multiple of 5 and even or odd

--- Lecture 2/practice.py
+++ Lecture 2/practice.py
@@ -1,3 +1,4 @@
+# User Authentication
 username = input("Enter the username: ")
 password = input("Enter the password: ")
 
@@ -7,3 +8,23 @@ elif (username != "admin"):
     print("Wrong Username!")
 else: 
     print("Wrong Password")
+    
+    
+# Check number is multiple of 5 or not
+
+n = int(input("Enter the value of n: "))
+
+if (n % 5 == 0): 
+    print(n, " is a multiple of 5")
+else:
+    print(n, " is not a multiple of 5")
+    
+    
+# Check the number is even or odd
+
+value = int(input("Enter the value: "))
+
+if (value % 2 == 0):
+    print("Even Number")
+else:
+    print("Odd Number")
