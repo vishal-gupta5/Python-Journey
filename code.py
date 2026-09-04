68e71a8acf4310f0891384d46e9cc35f4fd10ef5
vishal-gupta5 <guptaaryan1103@gmail.com>
2026-09-04 15:08:38 +0530

    Create variables in Python

--- code.py
+++ code.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+# Basic Syntex
+
+print("Hello World!") # Print Hello World
+print("Hello World ", "With Python") # Concate string with Space
+print("Vishal \nGupta") # Print in Next line
+
+# Variables
+
+name = "Vishal"
+age = 23
+PI = 3.14
+
+print("My name is:", name)
+print("My age is:", age)
+print("The value of PI is:", PI)
